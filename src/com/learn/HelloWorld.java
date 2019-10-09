e101b1806a0eafc76ab093d020b15e7db08b6bde
Keshav Giri <ikeshavgiri@gmail.com>
2019-10-09 21:20:35 +0530

    HelloWorld

---
+++ src/com/learn/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.learn;
+
+public class HelloWorld {    // Class HelloWorld
+     public static void main(String[] args) {   //Main method
+		System.out.println("Hello World!");   
+	}
+}
